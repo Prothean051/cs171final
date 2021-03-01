c48a7f164f6333f305a9fcb700df2c69dd04e644
Max Lee-Roller <mleeroller@gmail.com>
2021-02-28 17:54:06 -0800

    Init server.py

---
+++ server.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Assume server stuff is here. Just defining data structures and blockchain stuff for
+the eval.
+'''
+
+#Queue
+
+#Key-Value Store
+
+#Blockchain
+
+def bcReconstruct():
+    return None
+
+if __name__ == "__main__":
+    None
