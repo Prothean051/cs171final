f4deab009d050ef56166fc14432b7d6ef371a510
Max Lee-Roller <mleeroller@gmail.com>
2021-03-14 04:19:15 -0700

    Commands mostly working. Operations needs a final touch and rest might need to be changed for paxos implementation.

--- client.py
+++ client.py
@@ -18,6 +18,7 @@ sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socks = [sock1, sock2, sock3, sock4, sock5]
 
 sock1.connect((IP, SERVER_PORTS[0]))
 sock2.connect((IP, SERVER_PORTS[1]))
@@ -27,7 +28,7 @@ sock5.connect((IP, SERVER_PORTS[4]))
 
 
 def talk_to_server(sock, msg):
-    time.sleep(2)
+    time.sleep(5)
     sock.send(msg.encode())
     reply = sock.recv(1024).decode()
     print(reply)
@@ -46,9 +47,24 @@ def handle_input():
                 sock4.close()
                 os._exit(0)
 
-            elif user_input[0:4] == "send":
+            elif user_input[0:9] == "Operation": #Operation(put/get, key, val(only for put))
+                operation = user_input[9:].split(',')
+                op = operation[0].strip('(').strip()
+                if op == "get" or op == "put":
+                    key = operation[1].strip(')').strip()
+                    if len(operation)>2:
+                        val = operation[2].strip(')').strip()
+                        message = f"{op} {key} {val}"
+                    else: message = f"{op} {key}"
+                    #At this point, can send message to server. INCOMPLETE, currently sending manually for testing
+                else:
+                    print("Command is used as follows: Operation(put, key, value) or Operation(get, key).")
+
+            elif user_input[0:4] == "send": #send 'message' PID - can send to all with .
                 text = user_input.split("'")[1]
-                message = f"client message: [{text}]"
+                # message = f"client message: [{text}]"
+                message = text
+                print(f"Sending {message} to target server(s).")
                 if user_input[-1] == '1':
                     talk_to_server(sock1, message)
                 elif user_input[-1] == '2':
@@ -59,6 +75,9 @@ def handle_input():
                     talk_to_server(sock4, message)
                 elif user_input[-1] == '5':
                     talk_to_server(sock5, message)
+                elif user_input[-1] == '.':
+                    for sock in socks:
+                        talk_to_server(sock, message)
         except (KeyboardInterrupt, SystemExit):
             sock1.close()
             sock2.close()
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""Note for self, outstanding issues: 
+How/when to handle placing queue item into blockchain
+"""
 import socket
 import threading
 import sys
@@ -23,12 +26,15 @@ PORT_2 = other_ports[1]
 PORT_3 = other_ports[2]
 PORT_4 = other_ports[3]
 
+linkBroken = [False, False, False, False] #Really naive way of doing link fails, just check if the corresponding list entry is true or false before sending to a server
+
 # create sockets for this server & other servers
 my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socks = [sock1, sock2, sock3, sock4]
 
 def handle_client(sock, address):
     print(f"Accepted connection from {address}")
@@ -42,8 +48,16 @@ def handle_client(sock, address):
             sock.close()
             break
         print(f"Received: {data}")
-        if data[0:6] == 'client':
-            sock.send(f"Reply from server {PROCESS_ID}".encode())
+        # if data[0:6] == 'client':
+        #     sock.send(f"Reply from server {PROCESS_ID}".encode())
+        if data[0:3] == 'get':
+            op = data.split(' ')
+            response = getStudent(op[1])
+            sock.send(f"{response}".encode())
+        elif data[0:3] == 'put':
+            op = data.split(' ')
+            putStudent(op[1], op[2])
+            sock.send(f"Put operation ack'd, key: {op[1]}, val: {op[2]}".encode())
 
 def handle_input():
     global CONNECTED
@@ -61,6 +75,59 @@ def handle_input():
         elif user_input == "reconstruct":
             reconstruct()
 
+        elif user_input == "printBlockchain":
+            printBlockchain()
+
+        elif user_input == "printKVStore":
+            printKVStore()
+
+        elif user_input == "printQueue":
+            printQueue()
+
+        elif user_input == "failProcess":
+            print(f"Server {PROCESS_ID} intentional failed.")
+            os._exit(0)
+
+        elif user_input[0:8] == "failLink": #failLink(PID1, PID2). PID1 must be same as server that's running the command
+            cmd = user_input[8:].split(',')
+            cmd[0] = cmd[0].strip('(').strip()
+            cmd[1] = cmd[1].strip(')').strip()
+            if cmd[0] == PROCESS_ID and cmd[1] != PROCESS_ID:
+                if cmd[1] == "1":
+                    linkBroken[0] = True
+                elif cmd[1] == "2":
+                    if PROCESS_ID == "1": linkBroken[0] = True
+                    else: linkBroken[1] = True
+                elif cmd[1] == "3":
+                    if PROCESS_ID == "1" or PROCESS_ID == "2": linkBroken[1] = True
+                    else: linkBroken[2] = True
+                elif cmd[1] == "4":
+                    if PROCESS_ID == "5": linkBroken[3] = True
+                    else: linkBroken[2] = True
+                elif cmd[1] == "5":
+                    linkBroken[3] = True
+                print(f"Failed link between server {cmd[0]} and server {cmd[1]}.")
+
+        elif user_input[0:7] == "fixLink": #fixLink(PID1, PID2). PID1 must be same as server that's running the command
+            cmd = user_input[7:].split(',')
+            cmd[0] = cmd[0].strip('(').strip()
+            cmd[1] = cmd[1].strip(')').strip()
+            if cmd[0] == PROCESS_ID and cmd[1] != PROCESS_ID:
+                if cmd[1] == "1":
+                    linkBroken[0] = False
+                elif cmd[1] == "2":
+                    if PROCESS_ID == "1": linkBroken[0] = False
+                    else: linkBroken[1] = False
+                elif cmd[1] == "3":
+                    if PROCESS_ID == "1" or PROCESS_ID == "2": linkBroken[1] = False
+                    else: linkBroken[2] = False
+                elif cmd[1] == "4":
+                    if PROCESS_ID == "5": linkBroken[3] = False
+                    else: linkBroken[2] = False
+                elif cmd[1] == "5":
+                    linkBroken[3] = False
+                print(f"Fixed link between server {cmd[0]} and server {cmd[1]}.")
+
         elif not CONNECTED and user_input == "connect":
             CONNECTED = True
 
@@ -69,17 +136,20 @@ def handle_input():
             sock3.connect((IP, PORT_3))
             sock4.connect((IP, PORT_4))
 
-        elif CONNECTED and user_input[0:7] == "sendall":  # send all '[message goes in here]'
+        elif CONNECTED and user_input[0:7] == "sendall":  # sendall 'message'
             m = user_input.split("'")[1]
             message = f"[{m}] from P{PROCESS_ID}"
-            sock1.send(message.encode())
-            sock2.send(message.encode())
-            sock3.send(message.encode())
-            sock4.send(message.encode())
+            for i in range(len(socks)):
+                if linkBroken[i] == False:
+                    socks[i].send(message.encode())
+            
 
 #Queue. Stores operations on key-value store until they are added to the blockchain.
 ops = Queue()
 #example usage: ops.put(("put", "Joe Gaucho", "123-456-7890"))
+
+def printQueue():
+    print(list(ops.queue))
 
 #Key-Value Store. Using dict for now, but if disk persistency matters could use dbm.
 #Key is student netID/name, value is phone number. Access using put/getStudent.
@@ -91,8 +161,15 @@ def getStudent(key: str):
     ops.put(("get", key))
     return students[key]
 
+def printKVStore():
+    print(students)
+
 #Blockchain. Append-only chain of blocks.
 chain = [] #Using list for now. Access only using appendBlock. Each block is a tuple of size 3.
+
+def printBlockchain():
+    for block in chain:
+        print(blockToString(block))
 
 def convertQueue(): #pass this as target for a thread to automatically empty queue into blockchain
     while True:
@@ -136,12 +213,6 @@ def reconstruct(): #rebuild blockchain and keyvalue store from file
         hashpointer = splitline[-2]
         nonce = splitline[-1]
         appendBlock(op, hashpointer, nonce)
-    print("Blockchain rebuilt. Printing blockchain:")
-    for i in chain:
-        print(blockToString(i))
-    print("Printing students key-val store:")
-    for i in students:
-        print(i, students[i])   
 
 # thread working on std input
 input_thread = threading.Thread(daemon=True, target=handle_input)
@@ -152,7 +223,6 @@ my_sock.bind((IP, MY_PORT))
 my_sock.listen()
 
 while True:
-
     try:
         stream, addr = my_sock.accept()
         threading.Thread(daemon=True, target=handle_client, args=(
